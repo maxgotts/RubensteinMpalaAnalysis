64d1a673287dc8553829baf35d719f6c69a57df6
Max Gotts <mgotts@princeton.edu>
2021-07-09 11:31:55 -0700

    more exp1 stuff

--- Habitat_1/NDVI_Habitat_ANOVA.R
+++ Habitat_1/NDVI_Habitat_ANOVA.R
@@ -21,11 +21,15 @@ ggplot(hb, aes(x=Habitat.factor, y=NDVI, fill=Habitat.factor))+
   stat_summary(fun=mean, geom="point", shape=23, size=2, fill="black")+
   theme_bw()+
   scale_color_discrete(breaks=bushland,labels=bush.names)+
-  scale_fill_manual(values=c(ob,lb,mb))+
+  scale_fill_manual(values=c(og,lb,mb))+
   scale_x_discrete(limits=bushland,labels=bush.names)+
   labs(x="Habitat type",y="NDVI")+
   theme(legend.position="none")
 
+
+ggplot(hb, aes(x=NDVI))+
+  geom_histogram(fill=mb,color="black",binwidth=0.01)+
+  theme_classic()
 
 shapiro.test((hb %>% sample_n(5000))$GrassNDVI)
 (groups.compare <- kruskal.test(GrassNDVI ~ Habitat, data=hb)) #aov #kruskal.test
